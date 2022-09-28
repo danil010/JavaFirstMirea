4f1250da8595a06a68a3aeff1e97cf81f032b5bd
ivc1-5 <ddtemnikov@gmail.com>
2022-09-28 13:29:08 +0300

    прог

---
+++ src/ru/mirea/task2/tyu/Dog.java
@@ -0,0 +1,6 @@
+package ru.mirea.task2.tyu;
+
+public class Dog {
+    private String fullName;
+    private int age;
+}
---
+++ src/ru/mirea/task2/tyu/Shape.java
@@ -0,0 +1,12 @@
+package ru.mirea.task2.tyu;
+
+public class Shape {
+    public String toString()
+    {
+        return "Shape";
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
