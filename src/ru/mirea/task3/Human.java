8cdb419851298c29688a936f9e163216f56b97bd
ivc1-5 <ddtemnikov@gmail.com>
2022-09-30 11:35:23 +0300

    ty

--- src/ru/mirea/task3/Human.java
+++ src/ru/mirea/task3/Human.java
@@ -1,7 +1,34 @@
 package ru.mirea.task3;
 
-public class Human {
+import java.util.Scanner;
+
+public class Human
+{
    private Hand qHand;
    private Head pHead;
-   private Head Leg;
+   private Leg qLeg;
+   void regethu(Hand n ,Head i , Leg t)
+   {
+      qHand=n;
+      pHead=i;
+      qLeg=t;
+   }
+   void gethu()
+   {
+     // Scanner sc = new Scanner(System.in);
+      System.out.println(qHand.gethan());
+      System.out.println(pHead.gethea());
+      System.out.println( qLeg.getlg());
+
+   }
+   public static void main(String[] args)
+   {
+      Scanner sc = new Scanner(System.in);
+      Human ty = new Human();
+      ty.qHand.regethan(sc.nextInt());
+      ty.pHead.regethea(sc.nextInt());
+      ty.qLeg.regetlg(sc.nextInt());
+      ty.regethu(ty.qHand, ty.pHead,ty.qLeg);
+      ty.gethu();
+   }
 }
