193ab7651a8b47af7ffef1fd911a30598e6e9f6d
ivc1-5 <ddtemnikov@gmail.com>
2022-10-26 15:50:13 +0300

    ku

---
+++ src/ru/mirea/task8/MyDrawPanel.java
@@ -0,0 +1,10 @@
+package ru.mirea.task8;
+import javax.swing.*;
+import java.awt.*;
+
+public class MyDrawPanel extends JPanel {
+    public void paintComponent (Graphics g) {
+        Image image = new ImageIcon("C:\\Users\\ivc1-5\\IdeaProjects\\JavaFirstMirea\\src\\ru\\mirea\\task8\\082915-01.jpg"/*"C:\\Users\\temak\\IdeaProjects\\JavaFirstMirea\\src\\ru\\mirea\\task8\\image3.PNG"*/).getImage();
+        g.drawImage(image, 3, 4, this);
+    }
+}
